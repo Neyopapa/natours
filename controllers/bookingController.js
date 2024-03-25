8b6e105f86a65a0eea9878fc92d6959a7322bbe9
Omoniyi Osanyinlusi <cleverbrand1@gmail.com>
2024-03-25 12:58:52 +0100

    Updated

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -13,11 +13,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
 
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -62,7 +62,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  // const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.line_items[0].price_data.unit_amount / 100;
   await Booking.create({ tour, user });
 };
 
