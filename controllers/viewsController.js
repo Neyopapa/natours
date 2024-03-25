22233a8d543093026a358359a11887590b390524
Omoniyi Osanyinlusi <cleverbrand1@gmail.com>
2024-03-25 12:06:03 +0100

    Implemented stripe messages

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,6 +4,14 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const alert = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === 'booking')
+    res.locals.alert =
+      "Your booking was successful! please check your email for confirmation. if your booking doesn't show up here immediately, please come back later";
+  next();
+};
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from the collection
   const tours = await Tour.find();
