0f09edbbea11daa1b96e015cc4f53f1ab4fbe70e
Omoniyi Osanyinlusi <cleverbrand1@gmail.com>
2024-03-25 12:23:30 +0100

    Updated stripe information

--- public/js/index.js
+++ public/js/index.js
@@ -70,5 +70,5 @@ if (bookBtn)
     bookTour(tourId);
   });
 
-// const alertMessage = document.querySelector('body').dataset.alert;
-// if (alertMessage) showAlert('success', alertMessage, 20);
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
